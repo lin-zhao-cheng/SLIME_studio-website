11c3bc58f332c002344ce181f7de7ca7539bb48c
lin-zhao-cheng <jjaacckk41208@gmail.com>
2021-01-06 02:26:25 +0800

    首頁 icon變化完成 缺視差和選單

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
 $(document).ready(function () {
-    // let timeLine_page1 = new gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger: "#top2",
-    //       //yoyo: true,
-    //      // pin: true, // pin the trigger element while active
-    //       start: "center center", // when the top of the trigger hits the top of the viewport
-    //       end: "+=400 top", // end after scrolling 500px beyond the start
-    //      // pin:true,
-    //       scrub: true,
-    //       markers: true,
-    //       id: "top2", 
-    //     },
-    //   });
-    // timeLine_page1
-    // .to('#top2',1,{ opacity:0.6 });
-////////////////////////////////////////////////////////////////////////////////////////////
-    // let timeLine_page2 = new gsap.timeline({
-    //     scrollTrigger: {
-    //       trigger: "#top1",
-    //       //yoyo: true,
-    //       pin: true, // pin the trigger element while active
-    //       start: "center center", // when the top of the trigger hits the top of the viewport
-    //       end: "+=100", // end after scrolling 500px beyond the start
-    //       scrub: true,
-    //       markers: true,
-    //       id: "top1", 
-    //     },
-    //   });
-    // timeLine_page2
-    // .to('#top1',1);
-
+    new WOW().init();
+    
 })
